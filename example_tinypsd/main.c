4e41186566d82252d18341e4a0acaa5e93938aae
Tyler Pugmire <tyler.pugmire@yahoo.com>
2018-01-07 21:53:36 -0800

    initial example project and tinypsd.h
    added example project file that loads psd.
    reading the header of psd file now works.

--- example_tinypsd/main.c
+++ example_tinypsd/main.c
@@ -1,7 +1,14 @@
 #define TNYPSD_IMPLEMENTATION
 #include "tinypsd.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 int main(int argc, char** argv)
 {
+  tpsdPSD psd;
+  if (tpsdLoadPSD(&psd, "imgs/medium_mech_color_00.psd") == TPSD_LOAD_ERROR)
+  {
 
+  }
 }
--- tinypsd.h
+++ tinypsd.h
@@ -9,15 +9,146 @@
 
 #endif
 
+#define TPSD_LOAD_OK 1
+#define TPSD_LOAD_ERROR 0
 
+typedef struct tpsdHeader tpsdHeader;
+typedef struct tpsdPSD tpsdPSD;
+
+int tpsdLoadPSD(tpsdPSD* psd, char const* file);
+
+struct tpsdHeader
+{
+  int signature; //Signature: always equal to '8BPS' . Do not try to read the file if the signature does not match this value.
+  short version; //Version: always equal to 1. Do not try to read the file if the version does not match this value. (**PSB** version is 2.)
+  //6 bytes reserved here must be 0;
+  short numChannels; //The number of channels in the image, including any alpha channels.Supported range is 1 to 56.
+  unsigned height; //The height of the image in pixels. Supported range is 1 to 30,000. (**PSB** max of 300, 000.)
+  unsigned width; //The width of the image in pixels. Supported range is 1 to 30,000. (*PSB** max of 300, 000)
+  short depth; //Depth: the number of bits per channel. Supported values are 1, 8, 16 and 32.
+  short mode; //The color mode of the file. Supported values are: Bitmap = 0; Grayscale = 1; Indexed = 2; RGB = 3; CMYK = 4; Multichannel = 7; Duotone = 8; Lab = 9.
+};
+
+struct tpsdPSD
+{
+  tpsdHeader header;
+};
 
 ////   end header file   /////////////////////////////////////////////////////
 #endif /* TINYPSD_H */
 
 
+#define TINYPSD_IMPLEMENTATION
+
 #ifdef TINYPSD_IMPLEMENTATION
 
+#ifdef _WIN32
 
+#if !defined( _CRT_SECURE_NO_WARNINGS )
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <malloc.h> // alloca
+
+#else
+
+#include <alloca.h> // alloca
+
+#endif
+
+#include <stdio.h>  // fopen, fclose, etc.
+#include <stdlib.h> // malloc, free, calloc
+#include <string.h> // memcpy
+#include <assert.h> // assert
+
+#define TPSD_ASSERT assert
+#define TPSD_ALLOC malloc
+#define TPSD_ALLOCA alloca
+#define TPSD_FREE free
+#define TPSD_MEMCPY memcpy
+#define TPSD_CALLOC calloc
+
+static char* tpsdReadFileToMemory(const char* path, int* size)
+{
+  char* data = 0;
+  FILE* fp = fopen(path, "rb");
+  int sizeNum = 0;
+
+  if (fp)
+  {
+    fseek(fp, 0, SEEK_END);
+    sizeNum = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    data = (char*)TPSD_ALLOC(sizeNum + 1);
+    fread(data, sizeNum, 1, fp);
+    data[sizeNum] = 0;
+    fclose(fp);
+  }
+
+  if (size) *size = sizeNum;
+  return data;
+}
+
+unsigned short tpsdGetShort(char* data, int offset)
+{
+  return (short)(((short)data[offset]) << 8) | data[offset + 1];
+}
+
+unsigned tpsdGetInt(char* data, int offset)
+{
+  return (data[offset] << 24) | (data[offset + 1] << 16) | (data[offset + 2] << 8) | data[offset + 3];
+}
+
+unsigned short tpsdGetShortAndAdvance(char* data, int *offset)
+{
+  unsigned short num = (short)(((short)data[*offset]) << 8) | data[*offset + 1];
+  *offset += sizeof(short);
+  return num;
+}
+
+unsigned tpsdGetIntAndAdvance(char* data, int *offset)
+{
+  unsigned num = (data[*offset] << 24) | (data[*offset + 1] << 16) | (data[*offset + 2] << 8) | data[*offset + 3];
+  *offset += sizeof(unsigned);
+  return num;
+}
+
+tpsdHeader tpsdLoadPSDHeaderFromMemory(char* data, int* offset)
+{
+  tpsdHeader header = { 0 };
+  header.signature = tpsdGetIntAndAdvance(data, offset);
+  TPSD_ASSERT(header.signature == '8BPS');
+
+  header.version = tpsdGetShortAndAdvance(data, offset);
+  TPSD_ASSERT(header.version == 1);
+
+  *offset += 6; //6 bytes reserved here and must be zero;
+
+  header.numChannels = tpsdGetShortAndAdvance(data, offset);
+  header.height = tpsdGetIntAndAdvance(data, offset);
+  header.width = tpsdGetIntAndAdvance(data, offset);
+  header.depth = tpsdGetShortAndAdvance(data, offset);
+  header.mode = tpsdGetShortAndAdvance(data, offset);
+
+  return header;
+}
+
+int tpsdLoadPSDFromMemory(tpsdPSD* psd, char* data, int len)
+{
+  unsigned offset = 0;
+  psd->header = tpsdLoadPSDHeaderFromMemory(data, &offset);
+
+  return TPSD_LOAD_OK;
+}
+
+int tpsdLoadPSD(tpsdPSD* psd, const char* file)
+{
+  int size = 0;
+  char* data = tpsdReadFileToMemory(file, &size);
+  if (!data) return TPSD_LOAD_ERROR;
+  tpsdLoadPSDFromMemory(psd, data, size);
+  return TPSD_LOAD_OK;
+}
 
 #endif /* TINYPSD_IMPLEMENTATION */
 
